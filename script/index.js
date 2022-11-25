1bd2eef9b5263f29a048f2f2d21927459c6cdfa4
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-25 09:10:59 -0600

    created basic class

--- script/index.js
+++ script/index.js
@@ -23,5 +23,32 @@ function select(selector, parent = document) {
    console.log(content);
  }
 
+class Contact {
+    #name;
+    #city;
+    #email;
 
+    constructor(name, city, email) {
+        this.#name = name;
+        this.#city = city;
+        this.#email = email;
+    }
+
+    getName(){
+        this.#name = this.name
+        return this.#name
+    }
+
+    getCity(){
+        this.#city = this.city
+        return this.#name
+    }
+
+    getEmail(){
+        this.#email = this.email
+        return this.#email
+    }
+}
  
+
+const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
